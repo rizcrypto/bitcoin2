c1f314c9449842cdeb2dcdb0db0b9e0bcd4f6f8a
rizcrypto <rizcrypto@gmail.com>
2018-04-12 19:11:26 -0500

    Initial source code release

---
+++ src/P2p/P2pNetworks.h
@@ -0,0 +1,11 @@
+// Copyright (c) 2011-2016 The Cryptonote developers
+// Copyright (c) 2017-2018 B2N-project developers
+// Distributed under the MIT/X11 software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#pragma once
+
+namespace CryptoNote
+{
+  const static boost::uuids::uuid CRYPTONOTE_NETWORK = { { 0xb7, 0x50, 0x4f, 0xd4, 0xa1, 0x8d, 0x46, 0xaa, 0x81, 0x77, 0xe5, 0x83, 0x5a, 0x8c, 0x3e, 0x33 } };
+}
